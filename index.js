5971da86abbbbde4796b70a3c4d9f8900b639dd8
svar0 <svar0ne@hotmail.com>
2025-11-28 15:51:40 +0100

    added Media Queries and some more functions for the mystery button

--- index.js
+++ index.js
@@ -26,11 +26,37 @@ function form_validation() {
     return true
 }
 
+
+
+
+
 window.onload = function() {
-    this.document.getElementById("butn").onclick = func1;
+    document.getElementById("butn").onclick = func1;
 }
 
 function func1() {
     document.getElementById("form").innerHTML = "";
     document.getElementById("main_text").innerHTML = "<p>NOOO! what did you do??<br><br>why is my form gone??</p>"
+
+    document.getElementById("butn").onclick = func2;
+    document.getElementById("butn").style.marginTop = "10%"
+}
+
+function func2() {
+    const root = document.documentElement;
+    root.style.setProperty("--bgc0", "rgba(116, 116, 116, 1)");
+    root.style.setProperty("--bgc1", "rgb(218, 218, 218)");
+    root.style.setProperty("--bgc2", "rgba(163, 163, 163, 1)");
+    root.style.setProperty("--bgc3", "rgba(219, 219, 219, 1)");
+    root.style.setProperty("--bgc4", "rgba(186, 186, 186, 1)");
+    root.style.setProperty("--txtc", "black");
+
+    document.getElementById("main_text").innerHTML = "<p>AHHH! MY EYES!!<br><br> Is this light mode?! I hate light mode!!</p>"
+    document.getElementById("ftext").innerText = "@ICan'tBelieveYou'veDoneThis"
+
+    document.getElementById("butn").onclick = func3;
+}
+
+function func3() {
+
 }
