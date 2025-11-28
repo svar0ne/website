7d73fd131ce56c266c87afa69443586a7fa97449
svar0 <svar0ne@hotmail.com>
2025-11-28 21:28:16 +0100

    added more functions to mystery button

--- index.js
+++ index.js
@@ -58,5 +58,51 @@ function func2() {
 }
 
 function func3() {
+    document.getElementById("main_text").innerHTML = "<p>STOP PRESSING IT!!<br><br>can't you see you're breaking my website?!</p>"
+    document.getElementById("form").innerHTML = '<label for="n1">Name</label> <input id="n1"></input><br><label for="n2">Last name</label> <input id="n2"></input> <label for="n3">Last last name</label> <input id="n3"></input><br> <label for="n3">Lastest last name</label> <input id="n3"></input>'
 
+    document.getElementById("butn").onclick = func4;
+}
+
+function func4() {
+    document.getElementById("main_text").innerHTML = "<p>ボタンを押すのをやめてください!!<br><br>このウェブサイトには本当に一生懸命取り組んだ</p>"
+    document.getElementById("a1").innerText = "ホーム"
+    document.getElementById("a2").innerText = "何か"
+    document.getElementById("a3").innerText = "この"
+    document.getElementById("a4").innerText = "その"
+    document.getElementById("a5").innerText = "えええ"
+    document.getElementById("form").innerHTML = '<img src="bilder/face_angry_man4.png" alt="angry face" style="width: 150px; height: auto">'
+    document.getElementById("butn").innerText = "押さない!!"
+
+    document.getElementById("butn").onclick = func5;
+}
+
+function func5() {
+    document.getElementById("main_text").innerHTML = "<p>okay, that last one was weird...<br><br>Listen, I don't know what you're obsession is with this button, <br> but I've hidden it now, so could you just please go away?</p>"
+    document.getElementById("form").innerHTML = ""
+    document.getElementById("a1").innerText = "HOME"
+    document.getElementById("a2").innerText = "ELSE"
+    document.getElementById("a3").innerText = "THIS"
+    document.getElementById("a4").innerText = "THAT"
+    document.getElementById("a5").innerText = "OTHER"
+    document.getElementById("butn").innerText = "DON'T PRESS!"
+    document.getElementById("butn_a").style.marginLeft = "50%"
+    document.getElementById("butn").style.marginTop = "20%"
+    document.getElementById("butn").style.backgroundColor = "rgba(223, 223, 223, 1)"
+    document.getElementById("butn").style.color = "rgba(220, 217, 217, 1)"
+    document.getElementById("butn").style.border = "1px solid rgba(220, 220, 220, 1)"
+   
+    document.getElementById("butn").onclick = func6;
+}
+
+function func6() {
+    document.getElementById("butn_a").style.marginLeft = "0%"
+    document.getElementById("butn").style.marginTop = "10%"
+    document.getElementById("butn").style.backgroundColor = "rgba(255, 255, 255, 1)"
+    document.getElementById("butn").style.color = "red"
+    document.getElementById("butn").style.border = "0.5px solid black"
+    document.getElementById("butn").style.borderRadius = "2px"
+    document.getElementById("butn").style.padding = "8px"
+
+    document.getElementById("main_text").innerHTML = "<p><br><br></p>"
 }
